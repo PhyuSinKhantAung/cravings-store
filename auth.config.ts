815fe617ddbd5ceab9bddeedb7c9786ef11b663d
Phyu Sin Khant Aung <rollyphyusinkhant7290@gmail.com>
2024-04-20 23:45:12 +0630

    initialized next-auth

---
+++ auth.config.ts
@@ -0,0 +1,27 @@
+import type { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnProtectedRoute =
+        nextUrl.pathname.startsWith("/checkout") ||
+        nextUrl.pathname.startsWith("/orders");
+      if (isOnProtectedRoute) {
+        if (isLoggedIn) {
+          return true;
+        } else {
+          return false;
+        }
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      return true;
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
