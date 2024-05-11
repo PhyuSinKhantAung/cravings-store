e9172e77799796bde727faadb8649fe49c648be4
Phyu Sin Khant Aung <rollyphyusinkhant7290@gmail.com>
2024-05-12 00:34:51 +0630

    fixed some error issue to build and deploy

--- src/app/menus/[id]/page.tsx
+++ src/app/menus/[id]/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import React from "react";
 import SingleMenu from "@/app/components/menus/SingleMenu";
+
 export const getSingleMenuById = async (menuId: number) => {
   const menu = await prisma.menu.findUnique({
     where: {
@@ -12,11 +13,13 @@ export const getSingleMenuById = async (menuId: number) => {
   return menu;
 };
 
-const SingleMenuPage = async ({ params }: { params: { id: number } }) => {
+const SingleMenuPage = async ({ params }: { params: { id: string } }) => {
   const menu = await getSingleMenuById(Number(params.id));
 
   return (
-    <>{menu ? <SingleMenu menu={menu} /> : <p>Item is not avaliable</p>}</>
+    <div>
+      {menu ? <SingleMenu menu={menu} /> : <p>Item is not avaliable</p>}
+    </div>
   );
 };
 
